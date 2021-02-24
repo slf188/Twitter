50a06488ee5c62f0c1334884b9bc73d049783143
Felipe Vallejo <stephanovallejom@gmail.com>
2021-02-24 17:18:54 -0500

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Felipe Vallejo on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
